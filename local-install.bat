9e6e8437e7c85ca568beafee10815a657304a5d3
shukriev <dark00v3@gmail.com>
2018-04-26 17:21:40 +0300

    Local project openshift build script

---
+++ local-install.bat
@@ -0,0 +1,3 @@
+oc.exe login -u developer -p developer
+oc.exe project microservices-scrum
+oc.exe start-build gateway --from-dir=.
---
+++ local-install.sh
@@ -0,0 +1,3 @@
+oc login -u developer -p developer
+oc project microservices-scrum
+oc start-build gateway --from-dir=.
